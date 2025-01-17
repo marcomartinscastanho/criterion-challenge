3b842b6e1a7b3a78ff3ad772346ff2c590593e66
Marco Castanho <marco.castanho@gmail.com>
2025-01-17 22:06:48 +0000

    sort queries

--- 2025/category3-films.sql
+++ 2025/category3-films.sql
@@ -1,7 +1,7 @@
 INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 376 WHERE c.year = 2025 AND c.number = 3;
-INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 28835 WHERE c.year = 2025 AND c.number = 3;
+INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 712 WHERE c.year = 2025 AND c.number = 3;
 INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 952 WHERE c.year = 2025 AND c.number = 3;
 INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 955 WHERE c.year = 2025 AND c.number = 3;
-INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 712 WHERE c.year = 2025 AND c.number = 3;
-INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 28712 WHERE c.year = 2025 AND c.number = 3;
 INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 28427 WHERE c.year = 2025 AND c.number = 3;
+INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 28712 WHERE c.year = 2025 AND c.number = 3;
+INSERT INTO categories_category_films (category_id, film_id) SELECT c.id, f.cc_id FROM categories_category c JOIN films_film f ON f.cc_id = 28835 WHERE c.year = 2025 AND c.number = 3;
