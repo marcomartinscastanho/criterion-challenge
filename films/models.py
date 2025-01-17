6c4a1d0bb9d52e84a1e4f7c660dae9bebb6cf664
Marco Castanho <marco.castanho@gmail.com>
2025-01-17 14:38:23 +0000

    film country

--- films/models.py
+++ films/models.py
@@ -7,11 +7,68 @@ from directors.models import Director
 
 
 class Film(models.Model):
+    COUNTRY_CHOICES = [
+        ("AO", "Angola"),
+        ("AR", "Argentina"),
+        ("AU", "Australia"),
+        ("AT", "Austria"),
+        ("BD", "Bangladesh"),
+        ("BE", "Belgium"),
+        ("BR", "Brazil"),
+        ("CM", "Cameroon"),
+        ("CA", "Canada"),
+        ("CN", "China"),
+        ("CU", "Cuba"),
+        ("CZ", "Czechoslovakia"),
+        ("DK", "Denmark"),
+        ("ET", "Ethiopia"),
+        ("FI", "Finland"),
+        ("FR", "France"),
+        ("DE", "Germany"),
+        ("GR", "Greece"),
+        ("GT", "Guatemala"),
+        ("HK", "Hong Kong"),
+        ("HU", "Hungary"),
+        ("IS", "Iceland"),
+        ("IN", "India"),
+        ("ID", "Indonesia"),
+        ("IR", "Iran"),
+        ("IE", "Ireland"),
+        ("IT", "Italy"),
+        ("JP", "Japan"),
+        ("KZ", "Kazakhstan"),
+        ("LS", "Lesotho"),
+        ("MX", "Mexico"),
+        ("MA", "Morocco"),
+        ("NZ", "New Zealand"),
+        ("NG", "Nigeria"),
+        ("NO", "Norway"),
+        ("PH", "Philippines"),
+        ("PL", "Poland"),
+        ("PT", "Portugal"),
+        ("MK", "Republic of Macedonia"),
+        ("RO", "Romania"),
+        ("SN", "Senegal"),
+        ("KR", "South Korea"),
+        ("SU", "Soviet Union"),
+        ("ES", "Spain"),
+        ("SE", "Sweden"),
+        ("CH", "Switzerland"),
+        ("TW", "Taiwan"),
+        ("TH", "Thailand"),
+        ("TR", "Turkey"),
+        ("UA", "Ukraine"),
+        ("GB", "United Kingdom"),
+        ("US", "United States"),
+        ("WG", "West Germany"),
+        ("YU", "Yugoslavia"),
+    ]
     title = models.CharField(max_length=200)
     spine = models.PositiveIntegerField(unique=True, null=True, blank=True)
     year = models.PositiveIntegerField(validators=[MinValueValidator(1870)])
-    letterboxd = models.URLField(max_length=200)
+    country = models.CharField(max_length=2, choices=COUNTRY_CHOICES)
     directors = models.ManyToManyField(Director, related_name="films")
+    letterboxd = models.URLField(max_length=200)
 
     class Meta:
         ordering = ["title"]
