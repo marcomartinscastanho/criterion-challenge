6ae1370b69db506396ef4308a6612f774f56755b
Marco Castanho <marco.castanho@gmail.com>
2025-01-20 16:04:28 +0000

    login redirect to home if already logged in

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from home.views import home
+from home.views import LoginView, home
 
 urlpatterns = [
     path("", home, name="home"),
+    path("accounts/login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as DefaultLoginView
+from django.shortcuts import redirect, render
 
 
 @login_required
 def home(request):
     return render(request, "home.html")
+
+
+class LoginView(DefaultLoginView):
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("home")
+        return super().get(request, *args, **kwargs)
