55fe28a3d18893461abfde0e13cbf3a62d71d2a4
Marco Castanho <marco.castanho@gmail.com>
2025-01-21 16:35:56 +0000

    refactor categories view

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count, Prefetch, QuerySet
+from django.db.models import QuerySet
 from django.http import HttpRequest
 from django.shortcuts import render
 
@@ -10,33 +10,37 @@ from films.models import Film
 from users.models import User, UserWatched, UserWatchlist
 
 
-def parse_custom_criteria(criteria: dict, user: User):
-    films = Film.objects.all()
-    user_watched_qs = UserWatched.objects.filter(user=user).values("films")
-    user_watchlist_qs = UserWatchlist.objects.filter(user=user).values("films")
-    for key, value in criteria.items():
-        if key == "year" and value == "user__date_of_birth":
-            if not user.date_of_birth:
-                return Film.objects.none(), Film.objects.none(), Film.objects.none()
-            films = films.filter(year=user.date_of_birth.year)
-        if key == "spine" and value == "not_null":
-            films = films.filter(spine__isnull=False)
-        if key == "in" and value == "user__watchlist":
-            if not user_watchlist_qs:
-                return Film.objects.none(), Film.objects.none(), Film.objects.none()
-            films = films.filter(pk__in=user_watchlist_qs.values_list("films", flat=True))
-        if key == "director":
-            for key2, value2 in value.items():
-                if key2 == "not_in" and value2 == "user__films":
-                    if not user_watched_qs:
-                        return Film.objects.none(), Film.objects.none(), Film.objects.none()
-                    watched_directors = Director.objects.filter(
-                        films__in=user_watched_qs.values_list("films", flat=True)
-                    ).distinct()
-                    films = films.exclude(directors__in=watched_directors)
-    user_watched_films = films.filter(pk__in=user_watched_qs)
-    user_watchlist_films = films.filter(pk__in=user_watchlist_qs)
-    return films, user_watched_films, user_watchlist_films
+def get_category_films(
+    category: Category,
+    user: User,
+    user_watched_qs: QuerySet[UserWatched],
+    user_watchlist_qs: QuerySet[UserWatchlist],
+):
+    if category.films.exists():
+        films = category.films.all()
+    elif category.custom_criteria:
+        films = Film.objects.all()
+        for key, value in category.custom_criteria.items():
+            if key == "year" and value == "user__date_of_birth":
+                if not user.date_of_birth:
+                    return Film.objects.none(), Film.objects.none(), Film.objects.none()
+                films = films.filter(year=user.date_of_birth.year)
+            if key == "spine" and value == "not_null":
+                films = films.filter(spine__isnull=False)
+            if key == "in" and value == "user__watchlist":
+                if not user_watchlist_qs:
+                    return Film.objects.none(), Film.objects.none(), Film.objects.none()
+                films = films.filter(pk__in=user_watchlist_qs.values_list("films", flat=True))
+            if key == "director":
+                for key2, value2 in value.items():
+                    if key2 == "not_in" and value2 == "user__films":
+                        if not user_watched_qs:
+                            return Film.objects.none(), Film.objects.none(), Film.objects.none()
+                        watched_directors = Director.objects.filter(
+                            films__in=user_watched_qs.values_list("films", flat=True)
+                        ).distinct()
+                        films = films.exclude(directors__in=watched_directors)
+    return films
 
 
 def len_or_warning(qs: QuerySet):
@@ -46,36 +50,20 @@ def len_or_warning(qs: QuerySet):
 @login_required
 def categories(request: HttpRequest):
     user = request.user
-    user_watched_qs = UserWatched.objects.filter(user=user)
-    user_watchlist_qs = UserWatchlist.objects.filter(user=user)
-    categories = (
-        Category.objects.prefetch_related(
-            Prefetch("films"),
-            Prefetch("films__userwatched_set", queryset=user_watched_qs, to_attr="watched_for_user"),
-            Prefetch("films__userwatchlist_set", queryset=user_watchlist_qs, to_attr="watchlist_for_user"),
-        )
-        .filter(year=CURRENT_YEAR)
-        .annotate(films_count=Count("films", distinct=True))
-        .order_by("number")
-    )
+    user_watched_qs = UserWatched.objects.filter(user=user).values("films")
+    user_watchlist_qs = UserWatchlist.objects.filter(user=user).values("films")
     category_list = []
-    for category in categories:
-        films_qs = category.films.all()
-        watched_qs = []
-        watchlist_qs = []
-        if category.films.exists():
-            films_qs = category.films.all()
-            watched_qs = [film for film in films_qs if film.watched_for_user]
-            watchlist_qs = [film for film in films_qs if film.watchlist_for_user]
-        elif category.custom_criteria:
-            films_qs, watched_qs, watchlist_qs = parse_custom_criteria(category.custom_criteria, user)
+    for category in Category.objects.filter(year=CURRENT_YEAR).order_by("number"):
+        films = get_category_films(category, user, user_watched_qs, user_watchlist_qs)
+        user_watched_films = films.filter(pk__in=user_watched_qs)
+        user_watchlist_films = films.filter(pk__in=user_watchlist_qs)
         category_list.append(
             {
                 "number": category.number,
                 "title": category.title,
-                "films_count": len_or_warning(films_qs),
-                "watched_count": len(watched_qs),
-                "watchlist_count": len(watchlist_qs),
+                "films_count": len_or_warning(films),
+                "watched_count": len(user_watched_films),
+                "watchlist_count": len(user_watchlist_films),
             }
         )
     return render(request, "categories.html", {"categories": category_list})
