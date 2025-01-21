07361d5dd47c07925ebd843aa362a2dfab53c6d2
Marco Castanho <marco.castanho@gmail.com>
2025-01-21 16:19:27 +0000

    refactor categories query

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count, Prefetch
+from django.db.models import Count, Prefetch, QuerySet
 from django.http import HttpRequest
 from django.shortcuts import render
 
@@ -17,26 +17,30 @@ def parse_custom_criteria(criteria: dict, user: User):
     for key, value in criteria.items():
         if key == "year" and value == "user__date_of_birth":
             if not user.date_of_birth:
-                return "⚠️", "⚠️", "⚠️"
+                return Film.objects.none(), Film.objects.none(), Film.objects.none()
             films = films.filter(year=user.date_of_birth.year)
         if key == "spine" and value == "not_null":
             films = films.filter(spine__isnull=False)
         if key == "in" and value == "user__watchlist":
             if not user_watchlist_qs:
-                return "⚠️", "⚠️", "⚠️"
+                return Film.objects.none(), Film.objects.none(), Film.objects.none()
             films = films.filter(pk__in=user_watchlist_qs.values_list("films", flat=True))
         if key == "director":
             for key2, value2 in value.items():
                 if key2 == "not_in" and value2 == "user__films":
                     if not user_watched_qs:
-                        return "⚠️", "⚠️", "⚠️"
+                        return Film.objects.none(), Film.objects.none(), Film.objects.none()
                     watched_directors = Director.objects.filter(
                         films__in=user_watched_qs.values_list("films", flat=True)
                     ).distinct()
                     films = films.exclude(directors__in=watched_directors)
-    user_watched_films = user_watched_qs.filter(films__in=films).distinct()
-    user_watchlist_films = user_watchlist_qs.filter(films__in=films).distinct()
-    return films.count(), user_watched_films.count(), user_watchlist_films.count()
+    user_watched_films = films.filter(pk__in=user_watched_qs)
+    user_watchlist_films = films.filter(pk__in=user_watchlist_qs)
+    return films, user_watched_films, user_watchlist_films
+
+
+def len_or_warning(qs: QuerySet):
+    return len(qs) if qs.exists() else "⚠️"
 
 
 @login_required
@@ -56,23 +60,22 @@ def categories(request: HttpRequest):
     )
     category_list = []
     for category in categories:
+        films_qs = category.films.all()
+        watched_qs = []
+        watchlist_qs = []
         if category.films.exists():
-            films_count = category.films.count()
-            watched_count = sum(1 for film in category.films.all() if film.watched_for_user)
-            watchlist_count = sum(1 for film in category.films.all() if film.watchlist_for_user)
+            films_qs = category.films.all()
+            watched_qs = [film for film in films_qs if film.watched_for_user]
+            watchlist_qs = [film for film in films_qs if film.watchlist_for_user]
         elif category.custom_criteria:
-            films_count, watched_count, watchlist_count = parse_custom_criteria(category.custom_criteria, user)
-        else:
-            films_count = 0
-            watched_count = 0
-            watchlist_count = 0
+            films_qs, watched_qs, watchlist_qs = parse_custom_criteria(category.custom_criteria, user)
         category_list.append(
             {
                 "number": category.number,
                 "title": category.title,
-                "films_count": films_count,
-                "watched_count": watched_count,
-                "watchlist_count": watchlist_count,
+                "films_count": len_or_warning(films_qs),
+                "watched_count": len(watched_qs),
+                "watchlist_count": len(watchlist_qs),
             }
         )
     return render(request, "categories.html", {"categories": category_list})
