81d778d108e4233c173d5a5fd3ab3425a92c15cb
Marco Castanho <marco.castanho@gmail.com>
2025-01-22 17:23:45 +0000

    disable watched or picked options

--- picks/views.py
+++ picks/views.py
@@ -13,16 +13,32 @@ def picks(request: HttpRequest):
     user: User = request.user
     user_watched_qs = UserWatched.objects.filter(user=user).values("films")
     user_watchlist_qs = UserWatchlist.objects.filter(user=user).values("films")
-    # if not user.challenge_complete:
-    #     pass
+    if not user.challenge_complete:
+        pass  # TODO
     picks = []
-    for pick in Pick.objects.select_related("category").filter(user=user).filter(year=CURRENT_YEAR):
+    picks_qs = Pick.objects.select_related("category").filter(user=user).filter(year=CURRENT_YEAR)
+    for pick in picks_qs:
         all_category_films = get_category_films(pick.category, user, user_watched_qs, user_watchlist_qs)
+        category = pick.category
+        films = []
+        other_picks = picks_qs.exclude(pk=pick.pk)
+        for film in all_category_films:
+            is_picked = other_picks.values("film").filter(film=film).exists()
+            is_watched = user_watched_qs.filter(films=film).exists()
+            films.append(
+                {
+                    "cc_id": film.cc_id,
+                    "title": film.title,
+                    "year": film.year,
+                    "disabled": is_picked or is_watched,
+                }
+            )
         picks.append(
             {
+                "number": category.number,
                 "film": pick.film,
                 "watched": pick.watched,
-                "category": {"title": pick.category.title, "films": all_category_films},
+                "category": {"title": category.title, "films": films},
             }
         )
 
