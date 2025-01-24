88cd17cbe4d9ad868229afb8200c3424b087a217
Marco Castanho <marco.castanho@gmail.com>
2025-01-24 14:58:57 +0000

    use dotenv

--- criterionchallenge/settings.py
+++ criterionchallenge/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7t(n7z0$#^i#y(0%etn+yxk5bw9$rt_sp3dm!u4i20dv8&lsks"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", None) == "True"
 
 ALLOWED_HOSTS = []
 
