d7fe01717bf19e47189a59ab6455c11b6491514c
Marco Castanho <marco.castanho@gmail.com>
2025-02-19 12:05:26 +0000

    replace first name and last name for username

--- users/forms.py
+++ users/forms.py
@@ -6,14 +6,14 @@ from users.models import User, UserPreference
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "date_of_birth"]
+        fields = ["username", "date_of_birth"]
         widgets = {
-            "first_name": forms.TextInput(attrs={"class": "form-control"}),
-            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "username": forms.TextInput(attrs={"class": "form-control"}),
             "date_of_birth": forms.DateInput(attrs={"type": "date", "class": "form-control"}),
         }
         help_texts = {
-            "date_of_birth": 'Having your date of birth is important because the pool of available films in some categories depends on your birth year. E.g. "Watch a film from the year you were born"'
+            "username": "Should match your Letterboxd username.",
+            "date_of_birth": 'Having your date of birth is important because the pool of available films in some categories depends on your birth year. E.g. "Watch a film from the year you were born"',
         }
 
 
